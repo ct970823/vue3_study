ed787fc61e6beaf2d11512af95077528a58ed014
ct970823 <275493164@qq.com>
2021-06-08 09:20:06 +0800

    vite使用vant

--- vue-vite-test/src/App.vue
+++ vue-vite-test/src/App.vue
@@ -1,56 +1,373 @@
 <template>
-  <div>
-    <h3>toRef的使用及特点</h3>
-    <h4>state:{{ state }}</h4>
-    <h4>age:{{ age }}</h4>
-    <h4>money:{{ money }}</h4>
-    <hr />
-    <button @click="handleUpdate">更新</button>
-    <child></child>
-  </div>
+  <pull-refresh
+    class="van-pull-refresh"
+    v-model="isLoading"
+    success-text="刷新成功"
+    @refresh="onRefresh"
+  >
+    <div class="orderBox">
+      <div
+        class="orderList"
+        v-for="(item, index) in state.orderList"
+        :key="index"
+      >
+        <div class="orderListTop">
+          <Image width="80" height="80" radius="6" :src="item.picPath" />
+          <!-- <img
+            :src="item.picPath ? item.picPath : placeholderImg"
+            @error="(e) => imgError(e, index)"
+          /> -->
+          <div class="orderDetail">
+            <!-- 1->待付款；2->待发货；3->已发货；4->已完成；5->已关闭 -->
+            <div class="orderStatus">
+              <div>
+                {{
+                  item.status == 1
+                    ? item.timestamp <= 0
+                      ? "已取消"
+                      : "待支付"
+                    : item.status == 2
+                    ? "商家备货中"
+                    : item.status == 3
+                    ? "配送中"
+                    : item.status == 4
+                    ? "已完成"
+                    : item.status == 5
+                    ? "已取消"
+                    : "-"
+                }}
+              </div>
+            </div>
+            <count-down
+              :timestamp="item.timestamp"
+              boxStyle="margin-bottom: 8px;"
+              v-if="item.status == 1 && item.timestamp > 0"
+              @end="end"
+            ></count-down>
+            <div class="orderInfo">
+              <div class="orderInfoTitle">订单号：</div>
+              <div class="orderNUm">
+                {{ item.orderSn ? item.orderSn : "-" }}
+              </div>
+            </div>
+            <div
+              class="orderInfo"
+              v-if="!(item.status == 1 || item.status == 2 || item.status == 5)"
+            >
+              <div class="orderInfoTitle">运单号：</div>
+              <div class="orderLogistics">
+                {{
+                  item.status == 3
+                    ? item.expressSn
+                      ? item.expressSn
+                      : "待揽件"
+                    : item.expressSn
+                    ? item.expressSn
+                    : "-"
+                }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="orderBottom">
+          <a
+            :href="'tel:' + item.sellerLinkPhone"
+            v-if="item.status != 1"
+            :data-phone="item.sellerLinkPhone"
+            ><img src="../../../assets/img/shop/phone.png" />商家
+          </a>
+          <a
+            :href="'tel:' + item.driverPhone"
+            v-if="item.status != 1 && item.driverPhone"
+            ><img src="../../../assets/img/shop/phone.png" />小哥
+          </a>
+          <div
+            class="addressBtn"
+            v-if="item.status != 1"
+            @click="seeAddress(item.id)"
+          >
+            可视化配送
+          </div>
+          <div
+            class="payBtn"
+            v-if="item.status == 1 && item.timestamp > 0"
+            @click="pay(item.businessSn, item.timestamp, item.id)"
+          >
+            立即支付
+          </div>
+        </div>
+      </div>
+    </div>
+  </pull-refresh>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRef } from "vue";
-import Child from "./components/Child.vue";
+import { defineComponent, ref, reactive, onMounted } from "vue";
+import { PullRefresh, Loading, Image, Icon, Skeleton } from "vant";
+import axios from "axios";
 export default defineComponent({
   components: {
-    Child
+    PullRefresh,
+    Loading,
+    Image,
+    Icon,
+    Skeleton,
+  },
+  mounted() {
+    console.log(333333333);
   },
   setup() {
-    /*
-      toRef:
-        为源响应式对象上的某个属性创建一个 ref对象, 二者内部操作的是同一个数据值, 更新时二者是同步的
-        区别ref: 拷贝了一份新的数据值单独操作, 更新时相互不影响
-        应用: 当要将某个 prop 的 ref 传递给复合函数时，toRef 很有用
-    */
+    const isLoading = ref(false);
     const state = reactive({
-      age: 10,
-      money: 5
+      orderList: [],
     });
-    // 把state对象中的age属性创建一个ref对象 （同一数据源，更新时同步）
-    const age = toRef(state, "age");
-    // 把state对象中的money属性值变成一个ref对象（拷贝出来的新数据，更新时不影响）
-    const money = ref(state.money);
-    // 点击时属性值增加
-    const handleUpdate = () => {
-      age.value++;
-      money.value++;
-      console.log(state);
-      console.log(money);
-
-      // state.money++;
+    const pageNum = ref(1);
+    const pageSize = ref(10);
+    const total = ref(0);
+    const loadingMoreStatus = ref(false);
+    const appId = "wxad17f400ed2b7292";
+    const appSecret = "2bbecc9eedc9952f0ea1b7f54dcaa380";
+    const onRefresh = () => {
+      console.log(11111111);
+      // isLoading.value = true;
+      pageNum.value = 1;
+      getOrderList();
+      isLoading.value = false;
     };
+    const getOrderList = (flag = false) => {
+      console.log(11111111111);
 
+      const openId = "oYUz-joZfdhx78pkixS8KGYwdVrY";
+      axios
+        .post("/api/order/mallBuyerOrder/list", {
+          pageNum: pageNum.value,
+          pageSize: pageSize.value,
+          buyerOpenId: openId,
+        })
+        .then((res) => {
+          console.log(res);
+          // if (res.content.pageInfo.total == 0) {
+          //   this.$toast('期待您下单','center')
+          // }
+          res.data.content.data.forEach((item, index) => {
+            console.log(1);
+
+            item.isShow = !flag && index == 0;
+            // if (item.status == 1) {
+            const createTime = item.createTime;
+            const nowTime = new Date().getTime() / 1000;
+            item.timestamp = createTime + 900 - nowTime;
+            // }
+          });
+
+          let orderListItem = [];
+          orderListItem = flag
+            ? [...state.orderList, ...res.data.content.data]
+            : res.data.content.data;
+
+          state.orderList = orderListItem;
+          console.log(state.orderList);
+          total.value = res.data.content.pageInfo.total;
+          if (state.orderList.length >= total) {
+            loadingMoreStatus.value = false;
+          }
+        })
+        .catch((err) => {
+          // if(this.isRefresh){
+          //   this.$nextTick(()=>{
+          //     this.$refs.pullRefresh.moveState = 0
+          //   })
+          // }
+        })
+        .finally(() => {});
+    };
+    const debounce = (fn, delay = 500) => {
+      let timer = null;
+      return function () {
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          // console.log(this, arguments);
+          fn.apply(this, arguments);
+          timer = null;
+        }, delay);
+      };
+    };
+    onMounted(() => {
+      console.log("321321");
+      getOrderList();
+
+      // 上拉触底加载更多 使用防抖，防止多次加载
+      window.onscroll = debounce(() => {
+        //变量scrollTop是滚动条滚动时，距离顶部的距离
+        const scrollTop =
+          document.documentElement.scrollTop || document.body.scrollTop;
+        //变量windowHeight是可视区的高度
+        const windowHeight =
+          document.documentElement.clientHeight || document.body.clientHeight;
+        //变量scrollHeight是滚动条的总高度
+        const scrollHeight =
+          document.documentElement.scrollHeight || document.body.scrollHeight;
+        //滚动条到底部的条件
+        // console.log(scrollTop,windowHeight,scrollHeight)
+        if (scrollTop + windowHeight >= scrollHeight - 30) {
+          //到了这个就可以进行业务逻辑加载后台数据了
+          // _this.footerText = '我是有底线的';
+          if (total.value > state.orderList.length) {
+            pageNum.value += 1;
+            getOrderList(true);
+          }
+        }
+      }, 200);
+    });
     return {
+      isLoading,
+      onRefresh,
+      pageNum,
+      pageSize,
+      total,
+      getOrderList,
+      loadingMoreStatus,
       state,
-      age,
-      money,
-      handleUpdate
     };
-  }
+  },
 });
 </script>
 
 <style scoped>
+/* .van-pull-refresh {
+  position: relative;
+  height: 100vh;
+  overflow: auto;
+} */
+/* 订单列表 */
+.orderBox {
+  padding: 15px;
+}
+.orderTitle {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 15px;
+  border-radius: 6px;
+  color: #ffffff;
+}
+
+.orderList {
+  background-color: #ffffff;
+  padding: 15px;
+  border-radius: 6px;
+  overflow: hidden;
+  margin-bottom: 15px;
+}
+
+.openList {
+  height: 150px;
+}
+
+.cancelOpenList {
+  height: 110px;
+}
+
+.closeList {
+  height: 52px;
+}
+
+.orderBox > .orderList:last-child {
+  margin-bottom: 0;
+}
+
+.orderList > .orderListTop {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.orderList > .orderListTop > img {
+  width: 80px;
+  height: 80px;
+  border-radius: 6px;
+  flex: none;
+  transition: all 0.3s ease-in;
+}
+
+.closeList > .orderListTop > img {
+  width: 30px !important;
+  height: 30px !important;
+}
+
+.orderList > .orderListTop > .orderDetail {
+  margin-left: 16px;
+  overflow: hidden;
+}
+
+.orderList > .orderListTop > .orderDetail > .orderStatus {
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 14px;
+  transition: all 0.3s ease-in;
+  text-align: left;
+}
+
+.closeList > .orderListTop > .orderDetail > .orderStatus {
+  margin-top: 5px;
+}
+
+.orderList > .orderListTop > .orderDetail > .orderInfoTitle > .orderNum {
+  font-size: 11px;
+  margin-top: 14px;
+}
+
+.orderList > .orderListTop > .orderDetail > .orderInfo {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  font-size: 12px;
+  color: #757575;
+  margin-bottom: 4px;
+}
+
+.orderList > .orderListTop > .orderDetail > .orderInfo > .orderInfoTitle {
+  flex: none;
+}
+
+.orderList > .orderListTop > .orderDetail > .orderInfo > .orderNUm {
+  word-break: break-all;
+}
+
+.orderList > .orderListTop > .orderDetail > .orderInfo > .orderLogistics {
+  word-break: break-all;
+}
+
+.orderList > .orderBottom {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.orderList > .orderBottom > a,
+.orderList > .orderBottom > div {
+  width: 80px;
+  height: 24px;
+  border: 1px solid rgba(220, 232, 255, 1);
+  color: rgba(69, 129, 255, 100);
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 10px;
+  margin-top: 15px;
+}
+
+.orderList > .orderBottom > a > img {
+  width: 11px;
+  height: 14px;
+  margin-right: 4px;
+}
+
+.orderList > .orderBottom > .addressBtn {
+  width: 90px;
+  background-color: rgba(69, 129, 255, 1);
+  color: #ffffff;
+}
 </style>
--- vue-vite-test/vite.config.ts
+++ vue-vite-test/vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import vitePluginImp from 'vite-plugin-imp'
+// vite不需要配置组件的按需加载，因为Vant3 内部组件都是基于ESM编写的，但是样式需要配置按需加载
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'vant',
+          style(name) {
+            if (/CompWithoutStyleFile/i.test(name)) {
+              // This will not import any style file 
+              return false
+            }
+            return `vant/es/${name}/index.css`
+          }
+        },
+      ]
+    }),
+  ],
+  server: {
+    port: 3000,//启动端口
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'https://api.tes.e-dewin.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+    cors: true
+  }
 })
